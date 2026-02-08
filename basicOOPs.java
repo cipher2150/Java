1d9ee32f5f18bd2437617adbda3b20439a7cf040
Rajshekhar Prasad Saxena <121630495+cipher2150@users.noreply.github.com>
2026-02-08 18:09:57 +0530

    comments are formatted.

--- basicOOPs.java
+++ basicOOPs.java
@@ -1,4 +1,6 @@
-// OOP is a programming paradigm based on the concept of "objects", which can contain data and code: data in the form of fields (often known as attributes or properties), and code in the form of procedures (often known as methods). 
+// OOP is a programming paradigm based on the concept of "objects",
+//  which can contain data and code: data in the form of fields (often known as attributes or properties), 
+// and code in the form of procedures (often known as methods). 
 
 // Creating a class.
 class Animal {
