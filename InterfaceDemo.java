e26c4d99293fae6043bb164ba39d9657ed2f3ec0
Rajshekhar Prasad Saxena <121630495+cipher2150@users.noreply.github.com>
2026-06-04 22:37:32 +0530

    Fix comment formatting in InterfaceDemo.java

--- InterfaceDemo.java
+++ InterfaceDemo.java
@@ -1,4 +1,4 @@
-// An interface is a contract that a class can implement. 
+// An interface is a contract that a class can implement. ..
 // It defines a set of abstract methods that the implementing class must provide. 
 // Interfaces are used to achieve abstraction and multiple inheritance in Java.....
 
